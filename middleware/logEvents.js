0578bacc1477a68439c95e9ea6354a497b120b57
Ufuk Balaban <ufukkblbnn@gmail.com>
2022-10-29 23:35:40 +0300

    authorization part completed.
    authorization part completed.

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,4 +1,3 @@
-console.log("test!");
 
 //**npm i nodemon -g** => it monitors your files and when you save it automatically restarts the server so we're not always have to restart
 //**npm init **/
